831da982385b72613a22ff0cdafe789d3a86409a
Marko Boroš <Marko.Boros@vsite.hr>
2020-02-05 15:45:55 +0100

    Add project files.

---
+++ FAZM_Shows/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FAZM_Shows.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
